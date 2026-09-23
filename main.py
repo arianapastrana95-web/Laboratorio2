7c07f553e127580263136d0472d417e596c99332
Ariana Pastrana <arianapastrana95@gmail.com>
2026-09-22 21:02:33 -0500

    Req 1: Registrar datos básicos de una solicitud

---
+++ main.py
@@ -0,0 +1,9 @@
+def principal():
+
+    codigo_estudiante = input("Código de estudiante: ")
+    nombre_estudiante = input("Nombre completo del estudiante: ")
+    tipo_consulta = input("Tipo de consulta (matrícula, pagos, constancia, plataforma, otro): ")
+    descripcion_breve = input("Descripción breve de la solicitud: ")
+
+if __name__ == "__main__":
+    principal()

29b88bc15c06f211d56c306e64f54fb1cdd5d7e6
Ariana Pastrana <arianapastrana95@gmail.com>
2026-09-22 21:15:14 -0500

    Req 2: Validar longitud minima del codigo de estudiante

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 def principal():
-
+    
     codigo_estudiante = input("Código de estudiante: ")
+    while codigo_estudiante.strip() == "" or len(codigo_estudiante.strip()) < 5:
+        print("Error: El código no puede estar vacío y debe tener al menos 5 caracteres.")
+        codigo_estudiante = input("Código de estudiante válido: ")
     nombre_estudiante = input("Nombre completo del estudiante: ")
     tipo_consulta = input("Tipo de consulta (matrícula, pagos, constancia, plataforma, otro): ")
     descripcion_breve = input("Descripción breve de la solicitud: ")

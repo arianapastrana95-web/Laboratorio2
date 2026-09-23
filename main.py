8508403287e16f0d7369f3afb5a729f155e55f3b
Ariana Pastrana <arianapastrana95@gmail.com>
2026-09-22 21:46:06 -0500

    Req 5: Crear funcion con retorno para asignar prioridad

--- main.py
+++ main.py
@@ -4,19 +4,23 @@ def mostrar_menu():
     print("Módulo de Soporte Académico")
     print("-----------------------------------------")
 
+def asignar_prioridad(tipo_consulta):
+    if tipo_consulta in ["pagos", "plataforma"]:
+        return "Alta"
+    else:
+        return "Media"
+
 def principal():
-    
+
     mostrar_menu()
     
     codigo_estudiante = input("Código de estudiante: ")
+
     while codigo_estudiante.strip() == "" or len(codigo_estudiante.strip()) < 5:
         print("Error: El código no puede estar vacío y debe tener al menos 5 caracteres.")
         codigo_estudiante = input("Código de estudiante válido: ")
 
     nombre_estudiante = input("Nombre completo del estudiante: ")
-
-    tipo_consulta = input("Tipo de consulta (matrícula, pagos, constancia, plataforma, otro): ")
-    descripcion_breve = input("Descripción breve de la solicitud: ")
 
     consultas_validas = ["matrícula", "pagos", "constancia", "plataforma", "otro"]
     tipo_consulta = input("Tipo de consulta (matrícula, pagos, constancia, plataforma, otro): ").strip().lower()
@@ -26,6 +30,7 @@ def principal():
         tipo_consulta = input("Tipo de consulta (matrícula, pagos, constancia, plataforma, otro): ").strip().lower()
         
     descripcion_breve = input("Descripción breve de la solicitud: ")
+    prioridad = asignar_prioridad(tipo_consulta)
 
 if __name__ == "__main__":
     principal()
